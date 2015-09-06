34b65959b253291aa407e51c5333e6fa293a68bd
Paul <paullj1@gmail.com>
2015-09-05 22:53:42 -0400

    Adding helper functions

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -1,2 +1,15 @@
 module SettingsHelper
+	def competition_started?
+		start_time = Time.parse(Setting.find_by(name: 'start_time').value)
+		start_time < Time.zone.now
+	end
+	
+	def competition_ended?
+		end_time = Time.parse(Setting.find_by(name: 'end_time').value)
+		Time.zone.now > end_time
+	end
+
+	def competition_active?
+		competition_started? && !competition_ended?
+	end
 end
