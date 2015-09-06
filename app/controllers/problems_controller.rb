fbb77b3aef2c264053bf17711bd61204e17104e1
Paul <paullj1@gmail.com>
2015-09-06 10:09:25 -0400

    Redirects non-admins to root and non-logged in to login

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,5 +1,5 @@
 class ProblemsController < ApplicationController
-	before_action :logged_in_user, only: [:index, :show]
+	before_action :logged_in_user, only: [:index, :show, :destroy, :create, :index, :show, :edit, :update, :new]
 	before_action :admin_user, only: [:destroy, :create, :index, :show, :edit, :update, :new]
 	
 	def index
@@ -60,7 +60,7 @@ class ProblemsController < ApplicationController
       unless logged_in? && current_user.admin?
 				store_location
 				flash[:danger] = "Access Denied."
-				redirect_to login_url
+				redirect_to root_url
 			end
     end
 end
