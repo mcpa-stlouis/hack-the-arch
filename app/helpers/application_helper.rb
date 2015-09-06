67800e0747b80c450d0d35e701d77e4f583e916b
Paul <paullj1@gmail.com>
2015-09-06 09:55:48 -0400

    Adding dynamic comp name into title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	# Returns full title on per-page basis
   def full_title(page_title = '')
-    base_title = "HackTheArch"
+    base_title = competition_name
     if page_title.empty?
       base_title
     else
