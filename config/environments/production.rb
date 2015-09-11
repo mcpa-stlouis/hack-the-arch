bab778b45b555e2c247d81f2b9979cdfbdb91199
Paul <paullj1@gmail.com>
2015-09-11 19:25:47 -0400

    Updating sendgrid opts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
-	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   host = 'still-plateau-4408.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
