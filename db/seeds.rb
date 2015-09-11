b66716dc74c63f65fa5f2671b9e25011970f0271
Paul <paullj1@gmail.com>
2015-09-11 19:34:20 -0400

    race case fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,11 @@ User.create!(fname:  "admin",
 						 team_id: 1,
  						 activated_at: Time.zone.now)
 
-Team.create!(name:  "admins",
- 						 passphrase: "password",
- 						 members: "1")
-
 Setting.create!(name: "competition_name", value: "HackTheArch")
 Setting.create!(name: "max_members_per_team", value: "5")
 Setting.create!(name: "start_time", value: Time.zone.now.to_s)
 Setting.create!(name: "end_time", value: (Time.zone.now + 24.hours).to_s)
 
+Team.create!(name:  "admins",
+ 						 passphrase: "password",
+ 						 members: "1")
