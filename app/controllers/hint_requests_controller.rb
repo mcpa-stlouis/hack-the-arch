98cc6f9b5ccd057632ffab5713cfad63fb7afdd9
Paul <paullj1@gmail.com>
2015-09-28 10:25:21 -0400

    Fixing reversed logic in request

--- app/controllers/hint_requests_controller.rb
+++ app/controllers/hint_requests_controller.rb
@@ -11,7 +11,7 @@ class HintRequestsController < ApplicationController
 		@team = Team.find(@user.team_id)
 		hints_requested = @team.get_hints_requested(@problem.id)
 
-		if !@problem.solved_by?(@team.id)
+		if @problem.solved_by?(@team.id)
 			flash[:warning] = "Your team has already solved this challenge!"
 			redirect_to @problem
 		elsif ((!hints_requested || 
