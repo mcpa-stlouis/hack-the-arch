d42499c5b99ef6003b9ebb1375ee88cb103f0f14
Paul <paullj1@gmail.com>
2015-09-30 07:48:33 -0400

    Moving settings to beggining of seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+Setting.create!(name: "competition_name", value: "HackTheArch", setting_type: "text")
+Setting.create!(name: "max_members_per_team", value: "5", setting_type: "text")
+Setting.create!(name: "subtract_hint_points_before_solve", value: "true", setting_type: "boolean")
+Setting.create!(name: "scoreboard_on", value: "true", setting_type: "boolean")
+Setting.create!(name: "start_time", value: Time.zone.now.to_s, setting_type: "date")
+Setting.create!(name: "end_time", value: (Time.zone.now + 24.hours).to_s, setting_type: "date")
+
 bracket = Bracket.create!(name: "Professional", priority: "10")
           Bracket.create!(name: "College", priority: "5")
           Bracket.create!(name: "High School", priority: "1")
@@ -26,13 +33,6 @@ user = User.create!(id: Random.rand(10000),
  						 				activated_at: Time.zone.now)
 
 team.add(user)
-
-Setting.create!(name: "competition_name", value: "HackTheArch", setting_type: "text")
-Setting.create!(name: "max_members_per_team", value: "5", setting_type: "text")
-Setting.create!(name: "subtract_hint_points_before_solve", value: "true", setting_type: "boolean")
-Setting.create!(name: "scoreboard_on", value: "true", setting_type: "boolean")
-Setting.create!(name: "start_time", value: Time.zone.now.to_s, setting_type: "date")
-Setting.create!(name: "end_time", value: (Time.zone.now + 24.hours).to_s, setting_type: "date")
 
 hint = Hint.create!(hint: "The test was named after him",
 						 				points: "25",
