14ea36bb409fb5a19b4855fecb6d6aa6470166c1
Paul <paullj1@gmail.com>
2015-09-30 07:43:01 -0400

    No longer explicitly setting id in seed values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(id: Random.rand(10000),
-						 fname:  "admin",
- 						 lname: "user",
- 						 email: "admin@gmail.com",
- 						 password:              "password",
- 						 password_confirmation: "password",
- 						 admin: true,
- 						 activated: true,
-						 team_id: 1,
- 						 activated_at: Time.zone.now)
+
+bracket = Bracket.create!(name: "Professional", priority: "10")
+          Bracket.create!(name: "College", priority: "5")
+          Bracket.create!(name: "High School", priority: "1")
+
+team = Team.create!(name:  "admins",
+ 						 				passphrase: "password",
+ 						 				bracket_id: bracket.id)
+
+user = User.create!(id: Random.rand(10000),
+						 				fname:  "admin",
+ 						 				lname: "user",
+ 						 				email: "admin@gmail.com",
+ 						 				password:              "password",
+ 						 				password_confirmation: "password",
+ 						 				admin: true,
+ 						 				activated: true,
+						 				team_id: team.id,
+ 						 				activated_at: Time.zone.now)
+
+team.add(user)
 
 Setting.create!(name: "competition_name", value: "HackTheArch", setting_type: "text")
 Setting.create!(name: "max_members_per_team", value: "5", setting_type: "text")
@@ -23,27 +34,20 @@ Setting.create!(name: "scoreboard_on", value: "true", setting_type: "boolean")
 Setting.create!(name: "start_time", value: Time.zone.now.to_s, setting_type: "date")
 Setting.create!(name: "end_time", value: (Time.zone.now + 24.hours).to_s, setting_type: "date")
 
-Bracket.create!(id: "1", name: "Professional", priority: "10")
-Bracket.create!(id: "2", name: "College", priority: "5")
-Bracket.create!(id: "3", name: "High School", priority: "1")
+hint = Hint.create!(hint: "The test was named after him",
+						 				points: "25",
+						 				priority: "1")
 
-Team.create!(name:  "admins",
- 						 passphrase: "password",
- 						 bracket_id: "1",
- 						 members: "1")
-
-Problem.create!(id: "1",
-								name: "Who's that scientist?",
+Problem.create!(name: "Who's that scientist?",
 								points: "100",
 								category: "Trivia",
 								description: "What's the name of the scientist that invented the test to determine the ability of a machine to exhibit intelligent behavior equivalent to, or indistinguishable from, that of a human?",
     						solution: "Alan Turing",
     						correct_message: "Very good!",
     						false_message: "Try again!",
-    						hints: "1")
+    						hints: hint.id)
 
-Problem.create!(id: "2",
-								name: "Google Foo",
+Problem.create!(name: "Google Foo",
 								points: "100",
 								category: "Network Exploitation",
 								description: "What is the name of the site that indexes varios IoT devices like web cams and ethernet enabled thermostats?",
@@ -51,7 +55,3 @@ Problem.create!(id: "2",
     						correct_message: "Scary, right!?",
     						false_message: "Try again!")
 
-Hint.create!(id: "1",
-						 hint: "The test was named after him",
-						 points: "25",
-						 priority: "1")
