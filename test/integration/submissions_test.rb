7a9500c2fefc440133efdbfc0d07369cf497e7b0
Paul <paullj1@gmail.com>
2015-11-08 12:25:20 -0500

    Adding some submission tests

--- test/integration/submissions_test.rb
+++ test/integration/submissions_test.rb
@@ -3,6 +3,31 @@ require 'test_helper'
 class SubmissionsTest < ActionDispatch::IntegrationTest
 	def setup
 		@non_admin = users(:archer)
+		@problem = problems(:example_problem)
+	end
+
+	test "should add points when correct submission is made" do
+		log_in_as(@non_admin)
+		get problems_path
+		assert flash.empty?
+
+		@score = Team.find(@non_admin.team_id).get_score
+		assert_response :success
+    post submit_path, submission: { id: @problem.id, 
+																	 	value: @problem.solution }
+		assert_not_equal @score, Team.find(@non_admin.team_id).get_score
+	end
+
+	test "should not add points when incorrect submission is made" do
+		log_in_as(@non_admin)
+		get problems_path
+		assert flash.empty?
+
+		@score = Team.find(@non_admin.team_id).get_score
+		assert_response :success
+    post submit_path, submission: { id: @problem.id, 
+																	 	value: "  " }
+		assert_equal @score, Team.find(@non_admin.team_id).get_score
 	end
 		
 end
