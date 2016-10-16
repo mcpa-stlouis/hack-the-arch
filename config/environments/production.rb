538100bac3263a20e97fc25dbf86905da80aae17
Paul Jordan <paullj1@gmail.com>
2016-10-16 11:07:01 -0500

    Trying to set action_cable.url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  host = 'hta-demo.mcpa-stl.org'
+  host = 'hackthearch.herokuapp.com'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -37,7 +37,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://localhost/cable'
   config.action_cable.allowed_request_origins = [ 'localhost', 'localhost.ssl', host ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
