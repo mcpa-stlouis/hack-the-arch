a3e7c708dd957342a8b134d1f3dd156de52f52f8
Paul Jordan <paullj1@gmail.com>
2016-11-20 15:16:10 -0600

    Message broadcast job test

--- test/jobs/message_broadcast_job_test.rb
+++ test/jobs/message_broadcast_job_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class MessageBroadcastJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+    @message = messages(:test)
+	end
+
+  test 'should send message' do
+    MessageBroadcastJob.perform_now(@message)
+  end
 end
