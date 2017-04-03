e54bc3ea6f9345d35b54f05c381ae65b8e424158
Paul Jordan <paullj1@gmail.com>
2017-04-03 20:14:20 +0000

    Fix time compare on cool-down

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -25,7 +25,7 @@ class SubmissionsController < ApplicationController
     end
 
     # If the user is trying to brute force
-    unless (!current_user.last_submission || DateTime.now + 15.seconds < current_user.last_submission)
+    unless (!current_user.last_submission || (current_user.last_submission + 15.seconds) < DateTime.now)
       flash[:warning] = "Slow down!  You can only attempt to answer once every fifteen seconds!"
       redirect_to @problem
       return
